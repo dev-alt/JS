2c118c4198adac376973bc0f2e1a47431885aa8c
dev-alt <53392022+dev-alt@users.noreply.github.com>
2022-07-13 05:27:32 +1200

    first comment

---
+++ part1.js
@@ -0,0 +1,13 @@
+let lap1 = 34;
+let lap2 = 33;
+let lap3 = 36;
+
+function calclaps() {
+  console.log(lap1 + lap2 + lap3);
+}
+function lapstime() {
+  let total = lap1 + lap2 + lap3;
+  console.log(total);
+}
+calclaps();
+lapstime();
