6f64446d379848c26ec9e3601c5940c0a0f32ed0
dev-alt <53392022+dev-alt@users.noreply.github.com>
2022-07-17 00:09:01 +1200

    v1

--- part-4/Chrome/index.js
+++ part-4/Chrome/index.js
@@ -16,11 +16,10 @@ function renderLeads() {
   let listItems = "";
 
   for (let i = 0; i < myLeads.length; i++) {
-    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-    // const li = document.createElement("li");
-    // li.textContent = myLeads[i];
-    // ulEl.append(li);
-    listItems += "<li>" + myLeads[i] + "</li>";
+    listItems += `<li>
+        <a target='_blank' href='${myLeads[i]}'>
+        ${myLeads[i]}</a>
+      </li>`;
   }
   ulEl.innerHTML = listItems;
 }
--- part-4/index.js
+++ part-4/index.js
@@ -1,7 +1,24 @@
-const container = document.getElementById("container");
+// const container = document.getElementById("container");
 
-container.innerHTML = "<button onclick='buy()'>buy!</button>";
+// container.innerHTML = "<button onclick='buy()'>buy!</button>";
 
-function buy() {
-  container.innerHTML += "<p>Thank you for buying</p>";
-}
+// function buy() {
+//   container.innerHTML += "<p>Thank you for buying</p>";
+// }
+// ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+// const li = document.createElement("li");
+// li.textContent = myLeads[i];
+// ulEl.append(li);
+// listItems +=
+//   "<li><a href='" +
+//   myLeads[i] +
+//   "' target='_blank'>" +
+//   myLeads[i] +
+//   "</a></li>";
+// const recipient = "James";
+// const sender = "Andrew";
+
+// const email = `Hey ${recipient}!
+//     How is it going?
+//     cheers ${sender}`;
+// console.log(email);
