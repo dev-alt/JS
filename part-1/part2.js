2e77c667ab478fddcd81b844c00eebdd59aadd3d
dev-alt <53392022+dev-alt@users.noreply.github.com>
2022-07-22 18:23:52 +1200

    final

--- part-1/part2.js
+++ part-1/part2.js
@@ -10,9 +10,13 @@ function increment() {
 function save() {
   let countString = count + " - ";
   saveEl.textContent += countString;
+  total += count;
+  count = 0;
+  countEl.textContent = "Total: " + count;
 }
 function reset() {
   count = 0;
   saveEl.textContent = "";
   countEl.textContent = "";
+  total.textContent = "Total: ";
 }
